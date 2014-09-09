4b0c722da9aab0fc1dd39e304427e8fd3b2bed87
Denis R <student@alice.it>
2014-09-09 19:32:44 +0200

    Create strings.js

---
+++ nls/it/strings.js
@@ -0,0 +1,37 @@
+/*
+* Copyright (c) 2014 Travis Almand. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, browser: true */
+/*global define, brackets, $ */
+
+define({
+    'MENU_COMMAND': 'HTML Templates...',
+    'TITLE_H1': 'HTML Templates',
+    'NO_FILE_OPEN_ERROR': 'Non vi è alcun file aperto. Si prega di aprire un file prima di eseguire HTML Templates.',
+    'TITLE_H4': 'Scegli un modello...',
+    'CONTENT_ERROR': 'File ha un contenuto, questa azione sovrascrive il contenuto.',
+    'SECTION_DOCTYPE': 'Standard DOCTYPE',
+    'SELECT_LABEL': 'Scegli...',
+    'SECTION_TEMPLATES': 'Modelli Framework',
+    'SECTION_TEMPLATES_DESC': 'Questi modelli sono basati con HTML fornito con il framework, alcuni adeguamenti saranno necessari.',
+    'CANCEL_BUTTON': 'Cancella'
+});

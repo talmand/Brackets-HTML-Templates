ef61bd2f198e1eb846b7b5247c254f4a9cdb5a0c
Sean Wright <sean@wiredviews.com>
2014-10-08 13:45:06 -0400

    Fixed accidental re-format & added newlines to template files

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
 /*
- * Copyright (c) 2014 Travis Almand. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
+* Copyright (c) 2014 Travis Almand. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, browser: true */
 /*global define, require, brackets, $, Mustache, chosenTemplate */
 
 require.config({
     paths: {
-        "text": "lib/text",
-        "i18n": "lib/i18n"
+        "text" : "lib/text",
+        "i18n" : "lib/i18n"
     },
     locale: brackets.getLocale()
 });
 
 define(function (require, exports, module) {
-
+    
     'use strict';
 
-    var CommandManager = brackets.getModule('command/CommandManager'),
-        EditorManager = brackets.getModule('editor/EditorManager'),
-        Menus = brackets.getModule('command/Menus'),
-        Dialogs = brackets.getModule('widgets/Dialogs');
-
+    var CommandManager  = brackets.getModule('command/CommandManager'),
+        EditorManager   = brackets.getModule('editor/EditorManager'),
+        Menus           = brackets.getModule('command/Menus'),
+        Dialogs         = brackets.getModule('widgets/Dialogs');
+    
     // load up modal content, don't forget text! at beginning of file name
     var modal = require('text!html/modal.html');
     var Strings = require('strings');
-
+    
     function action() {
-
+        
         Dialogs.showModalDialogUsingTemplate(Mustache.render(modal, Strings));
-
+    
         var editor = EditorManager.getCurrentFullEditor();
         if (editor) {
             if (editor._codeMirror.getValue().length > 0) {
@@ -58,14 +58,14 @@ define(function (require, exports, module) {
             // no file is open, show error
             $('#templates_error').show();
         }
-
+        
         // result of clicking a template choice
         // selector is very specific to avoid cross-extension contamination, just in case
         $('#templates_modal select#standard, #templates_modal select#javascript, #templates_modal select#frameworks').on('change', function () {
             // send the chosen template
             chosenTemplate($(this).val());
         });
-
+        
         var chosenTemplate = function (choice) {
             // grab the html to be inserted into file
             var template;
@@ -89,7 +89,7 @@ define(function (require, exports, module) {
             case 'xhtml11':
                 template = require('text!html/xhtml11.html');
                 break;
-                // javascript
+            // javascript
             case 'javascriptIIFE':
                 template = require('text!html/javascriptIIFE.html');
                 break;
@@ -105,7 +105,7 @@ define(function (require, exports, module) {
             case 'angularDirective':
                 template = require('text!html/angularDirective.html');
                 break;
-                // frameworks
+            // frameworks
             case 'html5bp-4-3-0':
                 template = require('text!html/html5bp-4-3-0.html');
                 break;
@@ -121,20 +121,20 @@ define(function (require, exports, module) {
             default:
                 template = 'Something went wrong somewhere. Not horribly wrong, just wrong.';
             }
-
+            
             // insert html into file, this will overwrite whatever content happens to be there already
             EditorManager.getCurrentFullEditor()._codeMirror.setValue(template);
-
+            
             // automatically close the modal window
             $('#templates_modalBtn').click();
         };
 
     }
-
+    
     // Register the commands and insert in the File menu
     CommandManager.register(Strings.MENU_COMMAND, 'templates', action);
     var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
     menu.addMenuDivider();
     menu.addMenuItem('templates');
-
+    
 });
